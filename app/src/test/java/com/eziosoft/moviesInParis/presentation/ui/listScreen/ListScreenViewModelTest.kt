c6f304ec060c6a56255888e78e219542d32ce374
Bartosz Szczygiel <eziosoft@gmail.com>
2022-11-11 22:01:11 +0100

    viewModel tests (#10)

--- app/src/test/java/com/eziosoft/moviesInParis/presentation/ui/listScreen/ListScreenViewModelTest.kt
+++ app/src/test/java/com/eziosoft/moviesInParis/presentation/ui/listScreen/ListScreenViewModelTest.kt
@@ -1,65 +1,67 @@
-// package com.eziosoft.parisinnumbers.presentation.ui.listScreen
-//
-// import com.eziosoft.parisinnumbers.MainCoroutineRule
-// import com.eziosoft.parisinnumbers.domain.Movie
-// import com.eziosoft.parisinnumbers.domain.repository.OpenApiRepository
-// import com.eziosoft.parisinnumbers.domain.repository.TheMovieDbRepository
-// import com.eziosoft.parisinnumbers.navigation.ActionDispatcher
-// import com.eziosoft.parisinnumbers.navigation.SharedParameters
-// import com.eziosoft.parisinnumbers.presentation.ProjectDispatchers
-// import io.mockk.coEvery
-// import io.mockk.mockk
-// import kotlinx.coroutines.Dispatchers
-// import kotlinx.coroutines.ExperimentalCoroutinesApi
-// import kotlinx.coroutines.test.advanceUntilIdle
-// import kotlinx.coroutines.test.runTest
-// import org.junit.Rule
-// import org.junit.Test
-//
-// @OptIn(ExperimentalCoroutinesApi::class)
-// internal class ListScreenViewModelTest {
-//
-//    @get:Rule
-//    val rule = MainCoroutineRule()
-//
-//    private val sampleMovie = Movie(
-//        "id",
-//        "address",
-//        "year",
-//        "ardt",
-//        0.0,
-//        0.0,
-//        "startDate",
-//        "endDate",
-//        "placeId",
-//        "producer",
-//        "realisation",
-//        "title",
-//        "type"
-//    )
-//
-//    private val openApiRepo: OpenApiRepository = mockk {
-//        coEvery { getMovie(any()) } returns Result.success(sampleMovie)
-//    }
-//
-//    private val movieDbRepo: TheMovieDbRepository = mockk {
-//        coEvery { search(any()) } returns Result.success(emptyList())
-//    }
-//    private val actionDispatcher: ActionDispatcher = ActionDispatcher(SharedParameters())
-//    private val projectDispatchers = ProjectDispatchers(
-//        mainDispatcher = Dispatchers.Main,
-//        ioDispatcher = Dispatchers.Main
-//    )
-//
-//    @Test
-//    fun `search test`() = runTest {
-//        val viewModel = ListScreenViewModel(
-//            openApiRepo,
-//            movieDbRepo,
-//            actionDispatcher,
-//            projectDispatchers
-//        )
-//
-//        advanceUntilIdle()
-//    }
-// }
+package com.eziosoft.moviesInParis.presentation.ui.listScreen
+
+import com.eziosoft.moviesInParis.MainCoroutineRule
+import com.eziosoft.moviesInParis.domain.Movie
+import com.eziosoft.moviesInParis.domain.repository.DBState
+import com.eziosoft.moviesInParis.domain.repository.LocalDatabaseRepository
+import com.eziosoft.moviesInParis.navigation.Action
+import com.eziosoft.moviesInParis.navigation.ActionDispatcher
+import com.eziosoft.moviesInParis.navigation.SharedParameters
+import com.eziosoft.moviesInParis.presentation.ProjectDispatchers
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class ListScreenViewModelTest {
+
+    @get:Rule
+    val rule = MainCoroutineRule()
+
+    private val sampleMovie = Movie(
+        "id",
+        "address",
+        "year",
+        "ardt",
+        0.0,
+        0.0,
+        "startDate",
+        "endDate",
+        "placeId",
+        "producer",
+        "realisation",
+        "title",
+        "type"
+    )
+
+    private val dbRepo: LocalDatabaseRepository = mockk {
+        coEvery { getPaged(any(), any(), any()) } returns listOf(sampleMovie)
+        every { dbStateFlow } returns MutableStateFlow(DBState.Ready)
+    }
+    private val actionDispatcher: ActionDispatcher = ActionDispatcher(SharedParameters())
+    private val projectDispatchers = ProjectDispatchers(
+        mainDispatcher = Dispatchers.Main,
+        ioDispatcher = Dispatchers.Main
+    )
+
+    @Test
+    fun `check if db is queried when search`() = runTest {
+         ListScreenViewModel(
+            dbRepository = dbRepo,
+            actionDispatcher = actionDispatcher,
+            projectDispatchers = projectDispatchers
+         )
+
+        actionDispatcher.dispatchAction(Action.SearchMovie("movie"))
+        advanceUntilIdle()
+        coVerify { dbRepo.getPaged(any(), any(), "movie") }
+    }
+}
